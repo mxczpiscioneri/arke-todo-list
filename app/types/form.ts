1622e0ebc4f6a4fb2f67c7209e336184515e5253
Matheus Piscioneri <matheus.piscioneri@laborit.com.br>
2024-02-01 00:01:21 -0300

    chore: add types

---
+++ app/types/form.ts
@@ -0,0 +1,23 @@
+export interface InputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  type?: string;
+  className?: string;
+}
+
+export interface InputForm {
+  onAddTask: (name: string) => void;
+}
+
+export interface CheckboxForm {
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label?: string;
+}
+
+export interface Button {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+}
---
+++ app/types/task.ts
@@ -0,0 +1,19 @@
+export interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
+export interface TaskItem {
+  id: number;
+  name: string;
+  completed: boolean;
+  onToggleCompleted: (id: number) => void;
+  onRemove: (id: number) => void;
+}
+
+export interface TaskList {
+  tasks: Task[];
+  onToggleCompleted: (id: number) => void;
+  onRemoveTask: (id: number) => void;
+}
